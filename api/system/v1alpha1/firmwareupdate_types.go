bf4056b5ac362349d1c1f04661f60e8b47b12c4b
Nagadeesh Nagaraja <nagadeesh.nagaraja@sap.com>
2026-09-23 13:48:49 +0200

    fix(firmwareupdate): use scheme builder function pattern matching post-merge scaffold
    Signed-off-by: Nagadeesh Nagaraja <nagadeesh.nagaraja@sap.com>

--- api/system/v1alpha1/firmwareupdate_types.go
+++ api/system/v1alpha1/firmwareupdate_types.go
@@ -6,6 +6,7 @@ package v1alpha1
 import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/ironcore-dev/metal-maintenance-operator/api"
 	maintenancev1alpha1 "github.com/ironcore-dev/metal-maintenance-operator/api/maintenance/v1alpha1"
@@ -202,5 +203,8 @@ type FirmwareUpdateList struct {
 }
 
 func init() {
-	SchemeBuilder.Register(&FirmwareUpdate{}, &FirmwareUpdateList{})
+	SchemeBuilder.Register(func(s *runtime.Scheme) error {
+		s.AddKnownTypes(SchemeGroupVersion, &FirmwareUpdate{}, &FirmwareUpdateList{})
+		return nil
+	})
 }
